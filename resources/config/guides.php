86d584d0ff937a31d95a2af51df79fc4422269c9
Wouter de Jong <wouter@wouterj.nl>
2023-05-20 15:16:32 +0200

    Move config and templates to the respective packages

---
+++ resources/config/guides.php
@@ -0,0 +1,164 @@
+<?php
+
+declare(strict_types=1);
+
+use League\Tactician\CommandBus;
+use phpDocumentor\Guides\Compiler\Compiler;
+use phpDocumentor\Guides\Compiler\CompilerPass;
+use phpDocumentor\Guides\Compiler\DocumentNodeTraverser;
+use phpDocumentor\Guides\Compiler\NodeTransformer;
+use phpDocumentor\Guides\Compiler\NodeTransformers\CustomNodeTransformerFactory;
+use phpDocumentor\Guides\Compiler\NodeTransformers\NodeTransformerFactory;
+use phpDocumentor\Guides\Meta\ProjectMeta;
+use phpDocumentor\Guides\Metas;
+use phpDocumentor\Guides\NodeRenderers\DefaultNodeRenderer;
+use phpDocumentor\Guides\NodeRenderers\DelegatingNodeRenderer;
+use phpDocumentor\Guides\NodeRenderers\Html\DocumentNodeRenderer;
+use phpDocumentor\Guides\NodeRenderers\Html\MenuEntryRenderer;
+use phpDocumentor\Guides\NodeRenderers\Html\MenuNodeRenderer;
+use phpDocumentor\Guides\NodeRenderers\Html\SpanNodeRenderer;
+use phpDocumentor\Guides\NodeRenderers\Html\TableNodeRenderer;
+use phpDocumentor\Guides\NodeRenderers\InMemoryNodeRendererFactory;
+use phpDocumentor\Guides\NodeRenderers\NodeRendererFactory;
+use phpDocumentor\Guides\NodeRenderers\NodeRendererFactoryAware;
+use phpDocumentor\Guides\Parser;
+use phpDocumentor\Guides\References\ReferenceResolver;
+use phpDocumentor\Guides\References\Resolver\Resolver;
+use phpDocumentor\Guides\Renderer\HtmlRenderer;
+use phpDocumentor\Guides\Renderer\InMemoryRendererFactory;
+use phpDocumentor\Guides\Renderer\IntersphinxRenderer;
+use phpDocumentor\Guides\Renderer\LatexRenderer;
+use phpDocumentor\Guides\Renderer\TypeRendererFactory;
+use phpDocumentor\Guides\TemplateRenderer;
+use phpDocumentor\Guides\Twig\AssetsExtension;
+use phpDocumentor\Guides\Twig\EnvironmentBuilder;
+use phpDocumentor\Guides\Twig\ThemeManager;
+use phpDocumentor\Guides\Twig\TwigTemplateRenderer;
+use phpDocumentor\Guides\UrlGenerator;
+use phpDocumentor\Guides\UrlGeneratorInterface;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Reference;
+use Twig\Loader\FilesystemLoader;
+
+use function Symfony\Component\DependencyInjection\Loader\Configurator\param;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\tagged_iterator;
+
+return static function (ContainerConfigurator $container): void {
+    $container->parameters()
+        ->set('phpdoc.guides.base_template_paths', [__DIR__ . '/../../../guides/resources/template/html']);
+
+    $container->services()
+        ->defaults()
+        ->autowire()
+        ->autoconfigure()
+        ->instanceof(Resolver::class)
+        ->tag('phpdoc.guides.reference.resolver')
+
+        ->instanceof(NodeRendererFactoryAware::class)
+        ->tag('phpdoc.guides.noderendererfactoryaware')
+
+        ->instanceof(CompilerPass::class)
+        ->tag('phpdoc.guides.compiler.passes')
+
+        ->instanceof(NodeTransformer::class)
+        ->tag('phpdoc.guides.compiler.nodeTransformers')
+
+        ->load(
+            'phpDocumentor\\Guides\\Compiler\\NodeTransformers\\',
+            '%vendor_dir%/phpdocumentor/guides/src/Compiler/NodeTransformers/*Transformer.php',
+        )
+
+        ->load(
+            'phpDocumentor\\Guides\\Compiler\\Passes\\',
+            '%vendor_dir%/phpdocumentor/guides/src/Compiler/Passes/*Pass.php',
+        )
+
+        ->load(
+            'phpDocumentor\\Guides\\References\\Resolver\\',
+            '%vendor_dir%/phpdocumentor/guides/src/References/Resolver',
+        )->load(
+            'phpDocumentor\\Guides\\NodeRenderers\\',
+            '%vendor_dir%/phpdocumentor/guides/src/NodeRenderers',
+        )
+
+        ->set(Metas::class)
+        ->set(UrlGeneratorInterface::class, UrlGenerator::class)
+        ->set(ReferenceResolver::class)
+        ->arg('$resolvers', tagged_iterator('phpdoc.guides.reference.resolver'))
+
+        ->set(Parser::class)
+        ->arg('$parserStrategies', tagged_iterator('phpdoc.guides.parser.markupLanguageParser'))
+
+        ->set(Compiler::class)
+        ->arg('$passes', tagged_iterator('phpdoc.guides.compiler.passes'))
+
+        ->set(NodeTransformerFactory::class, CustomNodeTransformerFactory::class)
+        ->arg('$transformers', tagged_iterator('phpdoc.guides.compiler.nodeTransformers'))
+
+        ->set(DocumentNodeTraverser::class)
+
+        ->set(HtmlRenderer::class)
+        ->tag('phpdoc.renderer.typerenderer')
+        ->args(
+            ['$commandBus' => service(CommandBus::class)],
+        )
+        ->set(LatexRenderer::class)
+        ->tag('phpdoc.renderer.typerenderer')
+        ->args(
+            ['$commandBus' => service(CommandBus::class)],
+        )
+
+        ->set(IntersphinxRenderer::class)
+        ->tag('phpdoc.renderer.typerenderer')
+
+        ->set(DocumentNodeRenderer::class)
+        ->tag('phpdoc.guides.noderenderer.html')
+        ->set(SpanNodeRenderer::class)
+        ->tag('phpdoc.guides.noderenderer.html')
+        ->set(TableNodeRenderer::class)
+        ->tag('phpdoc.guides.noderenderer.html')
+        ->set(MenuNodeRenderer::class)
+        ->tag('phpdoc.guides.noderenderer.html')
+        ->set(MenuEntryRenderer::class)
+        ->tag('phpdoc.guides.noderenderer.html')
+
+        ->set(ProjectMeta::class)
+
+        ->set(InMemoryNodeRendererFactory::class)
+        ->args([
+            '$nodeRenderers' => tagged_iterator('phpdoc.guides.noderenderer.html'),
+            '$defaultNodeRenderer' => new Reference(DefaultNodeRenderer::class),
+        ])
+        ->alias(NodeRendererFactory::class, InMemoryNodeRendererFactory::class)
+
+         ->set(InMemoryRendererFactory::class)
+        ->arg('$renderSets', tagged_iterator('phpdoc.renderer.typerenderer'))
+        ->alias(TypeRendererFactory::class, InMemoryRendererFactory::class)
+
+
+        ->set(AssetsExtension::class)
+        ->arg('$nodeRenderer', service(DelegatingNodeRenderer::class))
+        ->tag('twig.extension')
+        ->autowire()
+
+        ->set(ThemeManager::class)
+        ->arg('$filesystemLoader', service(FilesystemLoader::class))
+        ->arg(
+            '$defaultPaths',
+            param('phpdoc.guides.base_template_paths'),
+        )
+
+        ->set(FilesystemLoader::class)
+        ->arg(
+            '$paths',
+            param('phpdoc.guides.base_template_paths'),
+        )
+
+        ->set(EnvironmentBuilder::class)
+        ->arg('$extensions', tagged_iterator('twig.extension'))
+        ->arg('$themeManager', service(ThemeManager::class))
+
+        ->set(TemplateRenderer::class, TwigTemplateRenderer::class)
+        ->arg('$environmentBuilder', new Reference(EnvironmentBuilder::class));
+};
---
+++ src/DependencyInjection/Compiler/NodeRendererPass.php
@@ -0,0 +1,114 @@
+<?php
+
+declare(strict_types=1);
+
+namespace phpDocumentor\Guides\DependencyInjection\Compiler;
+
+use phpDocumentor\Guides\NodeRenderers\NodeRendererFactory;
+use phpDocumentor\Guides\NodeRenderers\TemplateNodeRenderer;
+use phpDocumentor\Guides\Nodes\AnchorNode;
+use phpDocumentor\Guides\Nodes\CodeNode;
+use phpDocumentor\Guides\Nodes\DefinitionListNode;
+use phpDocumentor\Guides\Nodes\DefinitionLists\DefinitionNode;
+use phpDocumentor\Guides\Nodes\DocumentNode;
+use phpDocumentor\Guides\Nodes\FieldListNode;
+use phpDocumentor\Guides\Nodes\FigureNode;
+use phpDocumentor\Guides\Nodes\ImageNode;
+use phpDocumentor\Guides\Nodes\ListItemNode;
+use phpDocumentor\Guides\Nodes\ListNode;
+use phpDocumentor\Guides\Nodes\LiteralBlockNode;
+use phpDocumentor\Guides\Nodes\Metadata\AddressNode;
+use phpDocumentor\Guides\Nodes\Metadata\AuthorNode;
+use phpDocumentor\Guides\Nodes\Metadata\AuthorsNode;
+use phpDocumentor\Guides\Nodes\Metadata\ContactNode;
+use phpDocumentor\Guides\Nodes\Metadata\CopyrightNode;
+use phpDocumentor\Guides\Nodes\Metadata\DateNode;
+use phpDocumentor\Guides\Nodes\Metadata\MetaNode;
+use phpDocumentor\Guides\Nodes\Metadata\NoCommentsNode;
+use phpDocumentor\Guides\Nodes\Metadata\NoSearchNode;
+use phpDocumentor\Guides\Nodes\Metadata\OrganizationNode;
+use phpDocumentor\Guides\Nodes\Metadata\OrphanNode;
+use phpDocumentor\Guides\Nodes\Metadata\RevisionNode;
+use phpDocumentor\Guides\Nodes\Metadata\TocDepthNode;
+use phpDocumentor\Guides\Nodes\Metadata\TopicNode;
+use phpDocumentor\Guides\Nodes\Metadata\VersionNode;
+use phpDocumentor\Guides\Nodes\ParagraphNode;
+use phpDocumentor\Guides\Nodes\QuoteNode;
+use phpDocumentor\Guides\Nodes\RubricNode;
+use phpDocumentor\Guides\Nodes\SectionNode;
+use phpDocumentor\Guides\Nodes\SeparatorNode;
+use phpDocumentor\Guides\Nodes\TitleNode;
+use phpDocumentor\Guides\TemplateRenderer;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+
+final class NodeRendererPass implements CompilerPassInterface
+{
+    private const HTML = [
+        AnchorNode::class => 'inline/anchor.html.twig',
+        FigureNode::class => 'body/figure.html.twig',
+        MetaNode::class => 'structure/header/meta.html.twig',
+        ParagraphNode::class => 'body/paragraph.html.twig',
+        QuoteNode::class => 'body/quote.html.twig',
+        SeparatorNode::class => 'body/separator.html.twig',
+        TitleNode::class => 'structure/header-title.html.twig',
+        SectionNode::class => 'structure/section.html.twig',
+        DocumentNode::class => 'structure/document.html.twig',
+        ImageNode::class => 'body/image.html.twig',
+        CodeNode::class => 'body/code.html.twig',
+        DefinitionListNode::class => 'body/definition-list.html.twig',
+        DefinitionNode::class => 'body/definition.html.twig',
+        FieldListNode::class => 'body/field-list.html.twig',
+        ListNode::class => 'body/list/list.html.twig',
+        ListItemNode::class => 'body/list/list-item.html.twig',
+        LiteralBlockNode::class => 'body/literal-block.html.twig',
+        RubricNode::class => 'body/rubric.html.twig',
+        // Output as Metatags
+        AuthorNode::class => 'structure/header/author.html.twig',
+        CopyrightNode::class => 'structure/header/copyright.html.twig',
+        DateNode::class => 'structure/header/date.html.twig',
+        NoSearchNode::class => 'structure/header/no-search.html.twig',
+        TopicNode::class => 'structure/header/topic.html.twig',
+        // No output in page header in HTML - might be output in i.e. LaTex
+        AddressNode::class => 'structure/header/blank.html.twig',
+        AuthorsNode::class => 'structure/header/blank.html.twig',
+        ContactNode::class => 'structure/header/blank.html.twig',
+        NoCommentsNode::class => 'structure/header/blank.html.twig',
+        OrganizationNode::class => 'structure/header/blank.html.twig',
+        OrphanNode::class => 'structure/header/blank.html.twig',
+        RevisionNode::class => 'structure/header/blank.html.twig',
+        TocDepthNode::class => 'structure/header/blank.html.twig',
+        VersionNode::class => 'structure/header/blank.html.twig',
+    ];
+
+    public function process(ContainerBuilder $container): void
+    {
+        foreach ($container->findTaggedServiceIds('phpdoc.guides.noderendererfactoryaware') as $id => $tags) {
+            $definition = $container->getDefinition($id);
+            $definition->addMethodCall(
+                'setNodeRendererFactory',
+                [new Reference(NodeRendererFactory::class)],
+            );
+            $definition->clearTag('phpdoc.guides.noderendererfactoryaware');
+        }
+
+         $htmlRendererDefinitions = [];
+        foreach (self::HTML as $node => $template) {
+            $definition = new Definition(
+                TemplateNodeRenderer::class,
+                [
+                    '$renderer' => new Reference(TemplateRenderer::class),
+                    '$template' => $template,
+                    '$nodeClass' => $node,
+                ],
+            );
+            $definition->addTag('phpdoc.guides.noderenderer.html');
+
+            $htmlRendererDefinitions['phpdoc.guides.noderenderer.html.' . $node] = $definition;
+        }
+
+         $container->addDefinitions($htmlRendererDefinitions);
+    }
+}
